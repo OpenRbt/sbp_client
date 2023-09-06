9d6703ea60295584303d23b1430bbe086f9db9d8
Your Name <you@example.com>
2023-09-06 07:39:59 +0000

    postgres init updated

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,3 +1,3 @@
-CREATE USER wash_sbp_admin WITH PASSWORD 'wash_sbp_admin';
+CREATE USER wash_sbp_service WITH PASSWORD 'wash_sbp_password';
 CREATE DATABASE wash_sbp;
-GRANT ALL PRIVILEGES ON DATABASE wash_sbp TO wash_sbp_admin;
+GRANT ALL PRIVILEGES ON DATABASE wash_sbp TO wash_sbp_service;
