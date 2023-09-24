277251cc172a257f06a83dafd05eefd222ab3bc1
GnomVreditel <dyba.vladimir@gmail.com>
2023-09-24 10:25:01 +0400

    migrations

--- internal/repository/migrations/00004_init_update_index_for_washes.up.sql
+++ internal/repository/migrations/00004_init_update_index_for_washes.up.sql
@@ -1,4 +1,5 @@
 ALTER TABLE public.washes DROP CONSTRAINT unique_terminal_key_terminal_password;
 
-ALTER TABLE public.washes
-ADD CONSTRAINT unique_terminal_key_terminal_password UNIQUE (terminal_key, terminal_password) WHERE (deleted = false);
+CREATE UNIQUE INDEX idx_unique_terminal_key_terminal_password
+ON public.washes (terminal_key, terminal_password)
+WHERE (deleted = false)
